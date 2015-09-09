0e53c63140ce4b4892d9ccc26cee546c822c8fca
John McGovern <eaglevision20@gmail.com>
2015-09-09 09:29:10 -0500

    added contact list to contactbook route. started individual contact template. Added link to contactbook to header.

--- contact-book/app/pods/contact/model.js
+++ contact-book/app/pods/contact/model.js
@@ -1,10 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	user: DS.attr(),
+	user: DS.belongsTo('user', {async: true}),
 	firstName: DS.attr('string'),
 	lastName: DS.attr('string'),
 	imageUrl: DS.attr('string'),
 	email: DS.attr('string'),
 	phoneNumber: DS.attr('string'),
+	tags: DS.attr(),
+	groups: DS.attr()
 });
--- contact-book/app/pods/contactbook/controller.js
+++ contact-book/app/pods/contactbook/controller.js
@@ -4,4 +4,7 @@ export default Ember.Controller.extend({
 	user: function () {
         return this.store.findById('user', this.get('session.content.userId'));
     }.property('session'),
+    contacts: function () {
+    	return this.store.find('contact', {user: this.get('session.content.userId')});
+    }.property('session')
 });
